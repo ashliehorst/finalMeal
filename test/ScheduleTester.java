00ce5030425645da520273bb41176a875de0a779
Ashlie <Ashlie@Ashlie-PC>
2014-11-15 13:48:33 -0700

    Making Test for Schedule

--- test/ScheduleTester.java
+++ test/ScheduleTester.java
@@ -21,44 +21,52 @@ public class ScheduleTester {
     public ScheduleTester() {
     }
 
-    @Test
-    public void rotateRecipeListTest(){
+    
+    public void recipeTest() {
         Recipe spag = new Recipe();
-        spag.setTitle("Spaghetti");
+        spag.setTitle("1");
         s.getRecipeList().add(spag);
         
         Recipe food1 = new Recipe();
-        food1.setTitle("Spaghetti");
+        food1.setTitle("2");
         s.getRecipeList().add(food1);
         
         Recipe food2 = new Recipe();
-        food2.setTitle("Spaghetti");
+        food2.setTitle("3");
         s.getRecipeList().add(food2);
         
         Recipe food3 = new Recipe();
-        food3.setTitle("Spaghetti");
+        food3.setTitle("4");
         s.getRecipeList().add(food3);
         
         Recipe food4 = new Recipe();
-        food4.setTitle("Spaghetti");
+        food4.setTitle("5");
         s.getRecipeList().add(food4);
         
         Recipe food5 = new Recipe();
-        food5.setTitle("Spaghetti");
+        food5.setTitle("6");
         s.getRecipeList().add(food5);
         
         Recipe food6 = new Recipe();
-        food6.setTitle("Spaghetti");
+        food6.setTitle("7");
         s.getRecipeList().add(food6);
         
         Recipe food7 = new Recipe();
-        food7.setTitle("Spaghetti");
+        food7.setTitle("8");
         s.getRecipeList().add(food7);
-        
+    }
+    
+    @Test
+    public void rotateRecipeListTest(){
+        recipeTest();
         for (Recipe recipe : s.getRecipeList()){
             s.getRotateList().add(recipe);
+            System.out.println("Recipe: " + recipe.getTitle());
         }
         
+        for (Recipe rotate : s.getRotateList()) {
+            System.out.println("Rotate: " + rotate);
+        }
         s.rotateRecipes();
         
         Assert.assertNotEquals(s.iterateThruSchedule(), null);
