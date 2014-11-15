251d0d7c8bcff41604647fd0d3398d0794363ed6
poneilldev <p.oneill.apc@gmail.com>
2014-11-15 12:51:13 -0700

    Merge origin/master
    Conflicts:
    	src/TestClass.java

--- src/Schedule.java
+++ src/Schedule.java
@@ -6,6 +6,7 @@
 
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -20,6 +21,7 @@ public class Schedule {
     
     private List<Recipe> rotateList;
     private List<Recipe> recipeList;
+    private List<Recipe> scheduleList;
     private int count;
     private boolean flag;
     
@@ -30,6 +32,7 @@ public class Schedule {
     public Schedule() {
         recipeList = new ArrayList();
         rotateList = new ArrayList();
+        scheduleList = new ArrayList();
         count = 0;
         flag = false;
     }
@@ -70,10 +73,53 @@ public class Schedule {
     }
     
     /**
-     * START ADDING
+     * DAY TRACKER
+     * This method calls the rotateRecipes when Saturday 
+     * arrives
      */
-    private void startAdding(){
+    private void dayTracker(){
+        // if day is Saturday
+        if (Calendar.DAY_OF_WEEK == 6) {
+            rotateRecipes();
+        }
         
+    }
+    
+    /**
+     * ITETERATE THRU SCHEDULE LIST
+     * NOTE: The returned recipe is for today's meal
+     * Every time the program is opened, this 
+     * method needs to execute
+     * If null is returned then we have 
+     * a problem
+     * @return 
+     */
+    public Recipe iterateThruSchedule() {
+        int day = Calendar.DAY_OF_WEEK;
+        switch (day){
+            case 0:
+                // Sunday
+                return scheduleList.get(day);
+            case 1:
+                // Monday
+                return scheduleList.get(day);
+            case 2:
+                // Tuesday
+                return scheduleList.get(day);
+            case 3:
+                // Wednesday
+                return scheduleList.get(day);
+            case 4:
+                // Thursday
+                return scheduleList.get(day);
+            case 5:
+                // Friday
+                return scheduleList.get(day);
+            case 6:
+                // Saturday
+                return scheduleList.get(day);
+        }
+        return null;
     }
     
     /**
@@ -82,8 +128,19 @@ public class Schedule {
      * It needs to know the date Sun-Sat to know when to rotate
      * It needs to keep track of what recipe it last used so that it can start
      *   the new week on the next recipe.
+     * NOTE: The schedule will update Saturday Morning
      */
     public void rotateRecipes() {
+        // "i" keeps track of the seven days
+        // Count on number on rotateList
         
+        scheduleList.clear(); // clear every time before new week
+        for (int i = 0; i < 7; i++){
+            if (count == rotateList.size()) {
+                count = 0;
+            }
+            scheduleList.add(rotateList.get(count));
+            count++;
+        }
     }
 }
